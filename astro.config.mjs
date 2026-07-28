1c81fc107b85cfe5632590734800719b04d153ea
Yaqueline Maialy <sieras.yaqui94@gmail.com>
2026-07-28 16:22:06 -0300

    Fix: Corregir site y base

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,12 +11,12 @@ export default defineConfig({
   // URL completa de tu sitio de GitHub Pages o el que uses
   site: isGitHubActions
     ? "https://Yaqui-Sieras.github.io"
-    : "https://miResetCSS.vercel.app",
+    : "https://Yaqui-Sieras.vercel.app",
 
   // Nombre del repositorio (solo si NO es un repositorio de tipo usuario/organización principal)
   // Si se está compilando en GitHub Actions usa la subcarpeta,
   // si es en Vercel, Netlify o en local (npm run dev), usa la raíz '/'
-  base: isGitHubActions ? "/miResetCSS/" : "/",
+  base: isGitHubActions ? "/Porfolio/" : "/",
 
   integrations: [sitemap()],
 });
